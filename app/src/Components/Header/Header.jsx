5531ca007893cbc68b23592510ff73ce9f84d9ea
tcodes317 <tcodes317@gmail.com>
2024-05-10 18:32:58 -0700

    5/10/2024

--- app/src/Components/Header/Header.jsx
+++ app/src/Components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import FontAwesome from "font-awesome";
 function Header(){
     return (
         <>
@@ -10,10 +11,31 @@ function Header(){
                         <nav>
                             <a>Help</a>
                             <a>Login</a>
-                            <button>
+                            <button className="border-2 rounded-md">
                                 <span>Sign Up</span>
-                                
+                                <FaArrowRight />
                             </button>
+                            <div className="relative">
+                                <button className="flex items-center justify-between">
+                                    <span>EN</span>
+                                    <FaArrowRight />
+                                </button>
+                                <nav className="hidden">
+                                    <div>Deutsch</div>
+                                    <div>Espanol</div>
+                                    <div>Portugues</div>
+                                    <div>Russian</div>
+                                    <div>Turkce</div>
+                                    <div>Chineese</div>
+                                    <div>Dutch</div>
+                                    <div>Polski</div>
+                                    <div>Italiano</div>
+                                    <div>Francias</div>
+                                    <div>Bokmal</div>
+                                    <div>Thai</div>
+                                    <div>Vietnamese</div>
+                                </nav>
+                            </div>
                         </nav>
                     </div>
                 </header>
